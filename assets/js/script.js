1de73c32d00ae5a0d618e347129c1759fcf4781d
Jeferson Rosa Fagundes <jeferson.rf@outlook.com>
2021-11-11 00:39:41 -0300

    Desafio_2-Codelandia

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+    const sr = ScrollReveal({
+                origin: 'top',
+                distance: '50px',
+                duration: 2000,
+                reset: false
+            });
+    
+            ScrollReveal().reveal('.header_info', { delay: 200 });
+            ScrollReveal().reveal('.date', { delay: 400 });
+            ScrollReveal().reveal('.title', { delay: 500 });
+            ScrollReveal().reveal('.description', { delay: 600 }
+);
+
